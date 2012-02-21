691a753991704f60338383e604d22978d85178e3
Dave Woodall <dave@AMB3R.com>
2012-02-21 15:30:23 -0500

    learning the differences between Classes and Instances. Fun times.

--- day2/FizzBuzz.rb
+++ day2/FizzBuzz.rb
@@ -1,9 +1,10 @@
 (1..100).each do |i|
-  if i % 3 == 0 && i % 5 == 0
-    puts "Fizzbuzz is for #{i}"
-  elsif i % 3 == 0
-    puts "Fizz is for #{i}"
-  elsif i % 5 == 0
-    puts "Buzz is for #{i}"
+  print "#{i}:"
+  if i % 3 == 0
+    print "Fizz"
   end
+  if i % 5 == 0
+    print "Buzz"
+  end
+  puts "\n"
 end
