b7cd7caa111bbe833883e2975e33b2e542a78ac2
Jared Richardson <jared.r.richardson@gmail.com>
2012-02-23 09:27:06 -0500

    Adding a directory with files for the testing kata

---
+++ testing/test_card_deck.rb
@@ -0,0 +1,31 @@
+require 'test/unit'
+require 'CardDeck'
+
+class TestCardDeck < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_num_cards
+  end
+
+  def test_shuffle
+  end
+
+  def test_sort
+  end
+
+  def test_deal_one
+  end
+
+  def test_deal_20
+  end
+
+  def test_deal_too_many
+    # test card_deck size + 1
+  end
+
+end
