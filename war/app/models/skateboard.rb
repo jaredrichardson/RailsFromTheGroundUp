7662971fbc22e63402f8272111bab684070daf7f
Jared Richardson <jared.r.richardson@gmail.com>
2012-05-08 10:10:19 -0400

    Revert "Junk add"
    This reverts commit 8a99979b4898712943e909e97f33cdf8c3561f1e.

--- war/app/models/skateboard.rb
+++ war/app/models/skateboard.rb
@@ -3,6 +3,4 @@ class Skateboard < ActiveRecord::Base
   validates :wheel_color, :presence => true
   validates :style, :presence => true
   validates :size, :numericality => true
-
-  # has_many :wheel
 end
