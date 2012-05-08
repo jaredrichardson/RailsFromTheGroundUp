8a99979b4898712943e909e97f33cdf8c3561f1e
Jared Richardson <jared.r.richardson@gmail.com>
2012-05-08 10:08:21 -0400

    Junk add

--- war/app/models/skateboard.rb
+++ war/app/models/skateboard.rb
@@ -3,4 +3,6 @@ class Skateboard < ActiveRecord::Base
   validates :wheel_color, :presence => true
   validates :style, :presence => true
   validates :size, :numericality => true
+
+  # has_many :wheel
 end
